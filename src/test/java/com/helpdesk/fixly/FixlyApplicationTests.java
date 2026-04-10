9c249934d7a964c852bdec9ddfe97f5090bfff4e
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-04-09 21:57:03 -0300

    first commit

---
+++ src/test/java/com/helpdesk/fixly/FixlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.fixly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
