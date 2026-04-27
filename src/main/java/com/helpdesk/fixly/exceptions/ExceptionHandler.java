c92bc7d9cfd658c6af1235654a344b3646687cda
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-04-27 15:36:13 -0300

    BACKEND - Ajuste em importe de excessão

--- src/main/java/com/helpdesk/fixly/exceptions/ExceptionHandler.java
+++ src/main/java/com/helpdesk/fixly/exceptions/ExceptionHandler.java
@@ -3,14 +3,14 @@ package com.helpdesk.fixly.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandler {
 	
 	
-	@ExceptionHandler(NotFoundException.class)
+	@org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<StandarError> notFoundException(NotFoundException ex, HttpServletRequest request ){
 		
 		StandarError erro = new StandarError(System.currentTimeMillis(),
@@ -22,8 +22,8 @@ public class ExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 	
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<StandarError> dataIntegrityViolationException(NotFoundException ex, HttpServletRequest request ){
+	@org.springframework.web.bind.annotation.ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request ){
 		
 		StandarError erro = new StandarError(System.currentTimeMillis(),
 											 HttpStatus.BAD_REQUEST.value(),
