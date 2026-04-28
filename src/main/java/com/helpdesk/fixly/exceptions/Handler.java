858f678fb2a32d2a04d313cd6d5707b45e8711af
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-04-27 22:06:36 -0300

    BACKEND - ajuste

--- src/main/java/com/helpdesk/fixly/exceptions/Handler.java
+++ src/main/java/com/helpdesk/fixly/exceptions/Handler.java
@@ -3,14 +3,15 @@ package com.helpdesk.fixly.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
-public class ExceptionHandler {
+public class Handler {
 	
 	
-	@org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
+	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<StandarError> notFoundException(NotFoundException ex, HttpServletRequest request ){
 		
 		StandarError erro = new StandarError(System.currentTimeMillis(),
@@ -22,7 +23,7 @@ public class ExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 	
-	@org.springframework.web.bind.annotation.ExceptionHandler(DataIntegrityViolationException.class)
+	@ExceptionHandler(DataIntegrityViolationException.class)
 	public ResponseEntity<StandarError> dataIntegrityViolationException(DataIntegrityViolationException ex, HttpServletRequest request ){
 		
 		StandarError erro = new StandarError(System.currentTimeMillis(),
