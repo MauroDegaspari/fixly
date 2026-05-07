4534a5687926b302c0de0b0c94fea5839455de9e
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-05-07 16:34:31 -0300

    BACKEND - ajustes requisições http  CHAMADOS

--- src/main/java/com/helpdesk/fixly/controllers/ChamadoController.java
+++ src/main/java/com/helpdesk/fixly/controllers/ChamadoController.java
@@ -1,12 +1,18 @@
 package com.helpdesk.fixly.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.helpdesk.fixly.dtos.ChamadosDto;
+import com.helpdesk.fixly.dtos.ClienteDto;
+import com.helpdesk.fixly.models.ChamadosModel;
 import com.helpdesk.fixly.services.ChamadosService;
 
 @RestController
@@ -17,12 +23,23 @@ public class ChamadoController {
 	private ChamadosService service;
 	
 	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadosDto> chamadoById(@PathVariable Integer id){
+		
+		ChamadosModel chamadoId = service.acharChamado(id);
+		
+		return ResponseEntity.ok().body(new ChamadosDto(chamadoId	));
+	}
+	
+	
+	
 	@GetMapping
-	public ResponseEntity<ChamadosDto> todosChamados(){
+	public ResponseEntity<List<ChamadosDto>> todosChamados(){
 		
-		//ChamadosModel chamados = new ChamadosModel() service.todosChamados();
+		List<ChamadosModel> chamados =  service.todosChamados();
+		List<ChamadosDto> chamaddosDto = chamados.stream().map(x -> new ChamadosDto(x)).collect(Collectors.toList());
 		
-		return null;
+		return ResponseEntity.ok().body(chamaddosDto);
 	}
 
 }
--- src/main/java/com/helpdesk/fixly/services/ChamadosService.java
+++ src/main/java/com/helpdesk/fixly/services/ChamadosService.java
@@ -1,16 +1,32 @@
 package com.helpdesk.fixly.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.helpdesk.fixly.exceptions.NotFoundException;
 import com.helpdesk.fixly.models.ChamadosModel;
+import com.helpdesk.fixly.reposistories.ChamadosRepository;
 
 @Service
 public class ChamadosService {
 
-	public List<ChamadosModel> todosChamados(){
-		return null;
+	@Autowired
+	private ChamadosRepository repo;
+	
+	public ChamadosModel acharChamado(Integer id) {
+		Optional<ChamadosModel> chamadoId = repo.findById(id);
+		return chamadoId.orElseThrow(() -> new  NotFoundException("Chamado não encontrado pelo ID: "+ id));
 	}
+	
+	public List<ChamadosModel> todosChamados(){
+		
+		List<ChamadosModel> todosChamados = repo.findAll();
+		
+		return todosChamados;
+	}
+
 	
 }
