7d5f24f91114fd07cdc9ff32ca6bdea3c97416a8
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-05-25 12:03:09 -0300

    FRONTEND - Importes necessarios

--- fixly-front/src/app/app-module.ts
+++ fixly-front/src/app/app-module.ts
@@ -4,13 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 
+// Trabalhar com formularios
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+// Para realizar Requisições  HTTP
+import { HttpClient } from '@angular/common/http';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 @NgModule({
   declarations: [
     App
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatTableModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
