b000aa022aefbda662a8c7934b87f24412bca0be
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-05-28 11:32:59 -0300

    FRONTENTD - criação de model para tecnico e ajuste em http e chamada

--- fixly-front/src/app/components/tecnicos/tec-list/tec-list.ts
+++ fixly-front/src/app/components/tecnicos/tec-list/tec-list.ts
@@ -1,6 +1,7 @@
 import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from '../../../models/tecnicos';
 
 @Component({
   selector: 'app-tec-list',
@@ -11,9 +12,23 @@ import { MatTableDataSource } from '@angular/material/table';
 
 export class TecList implements AfterViewInit {
 
-  displayedColumns: string[] = ['position', 'name','email', 'perfil', 'symbol'];
+  ELEMENT_DATA: Tecnico[] = [
+    
+    {
+      id: 1,
+      nome: 'Mauro Degaspari',
+      cpf:'000000000',
+      email: 'mauro_degaspari@hotmail.com',
+      senha: 'teste',
+      perfis: ['0'],
+      dataCriacao: '27/09/2020' 
+    }
 
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  ];
+
+  displayedColumns: string[] = ['id', 'name','email', 'perfil', 'acao'];
+
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
@@ -24,32 +39,3 @@ export class TecList implements AfterViewInit {
 
 }
 
-export interface PeriodicElement {
-  name: string;
-  email: string;
-  position: number;
-  perfil: number;
-  symbol: string;
-}
-
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', email: 'Hyoga@email.com', perfil: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', email: 'Hyoga@email.com', perfil: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', email: 'Hyoga@email.com',perfil: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', email: 'Hyoga@email.com', perfil: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', email: 'Hyoga@email.com', perfil: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', email: 'Hyoga@email.com', perfil: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', email: 'Hyoga@email.com', perfil: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', email: 'Hyoga@email.com', perfil: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', email: 'Hyoga@email.com', perfil: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', email: 'Hyoga@email.com', perfil: 20.1797, symbol: 'Ne'},
-  {position: 11, name: 'Sodium', email: 'Hyoga@email.com', perfil: 22.9897, symbol: 'Na'},
-  {position: 12, name: 'Magnesium', email: 'Hyoga@email.com', perfil: 24.305, symbol: 'Mg'},
-  {position: 13, name: 'Aluminum', email: 'Hyoga@email.com', perfil: 26.9815, symbol: 'Al'},
-  {position: 14, name: 'Silicon', email: 'Hyoga@email.com', perfil: 28.0855, symbol: 'Si'},
-  {position: 15, name: 'Phosphorus', email: 'Hyoga@email.com', perfil: 30.9738, symbol: 'P'},
-  {position: 16, name: 'Sulfur', email: 'Hyoga@email.com', perfil: 32.065, symbol: 'S'},
-  {position: 17, name: 'Chlorine', email: 'Hyoga@email.com', perfil: 35.453, symbol: 'Cl'},
-
-];
