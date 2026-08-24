78b1f3377be2d47b2a48447100ab434174aabff7
MauroDegaspari <mauro_degaspari@hotmail.com>
2026-08-24 14:06:20 -0300

    FRONT - login na aplicação

--- fixly-front/src/app/components/login/login.ts
+++ fixly-front/src/app/components/login/login.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Credenciais } from '../../models/credenciais';
 import { FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { Auth } from '../../services/auth';
 
 @Component({
   selector: 'app-login',
@@ -19,15 +20,22 @@ credenciais: Credenciais = {
 email = new FormControl(null, Validators.email);
 senha = new FormControl(null, Validators.minLength(3));
 
-constructor(private toastr: ToastrService){}
+constructor(private toastr: ToastrService, private service: Auth){}
 
-ngOnInit(): void{
-}
+ngOnInit(): void{}
 
 
  logar() {
-  this.toastr.error('Login inválido!', 'Erro');
-  this.senha.reset();
+  this.service.authenticate(this.credenciais).subscribe(
+    (response) => {
+      this.toastr.success('Login realizado com sucesso!', 'Sucesso');
+      console.log(response);
+    },
+    (error) => {
+      this.toastr.error('Erro ao realizar login', 'Erro');
+      console.log(error);
+    }
+  );
 
 }
 
---
+++ fixly-front/src/app/services/auth.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Credenciais } from '../models/credenciais';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../config/config';
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Auth {
+
+  constructor(private http: HttpClient) {}
+
+  authenticate( creds: Credenciais ){
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    })
+  
+  }
+}
+  
